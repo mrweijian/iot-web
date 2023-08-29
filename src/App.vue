3fd359052faddd7c1e2e45e08c6f2371099663c2
weijian <1026760118@qq.com>
2023-08-29 09:54:12 +0800

    取消浏览器刷新验证

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@
 </template>
 
 <script setup lang="ts">
-
-onMounted(() => {
-  window.addEventListener('onubload', (event) => {
-    // 此处可以执行一些代码，例如询问用户是否确定离开页面
-    console.log("重新加载")
-    return false;
-  })
-})
 </script>
 
 <style scoped lang="less">
